23035b83fc93e124e20f8d8d108a2eb130fb4018
hyh <ec.huyinghuan@gmail.com>
2017-07-26 09:11:46 +0800

    :sparkles: 增加公共组建图片copy

--- index.js
+++ index.js
@@ -95,4 +95,18 @@ exports.registerPlugin = function(cli, options){
     next()
   }, 50)
 
+  cli.registerHook('build:end', (buildConfig, cb)=>{
+    for(let key in cli.options.pluginsConfig){
+      if(key.indexOf('sp') == 0){
+        continue
+      }
+      let moduleImagesDir = _path.join(cli.cwd(), cli.options.pubModulesDir, key, "image")
+      let outputImageDir =  _path.join(cli.options.buildConfig.outdir, "image", key)
+      if(_fs.existsSync(moduleImagesDir)){
+        _fs.copySync(moduleImagesDir, outputImageDir)
+        cli.log.info(`pub modules copy dir '${key}/image' to '/image/${key}'`)
+      } 
+    }
+    cb(null)
+  }, 1)
 }
